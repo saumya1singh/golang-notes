384ffa44b42f826ad333cce4203de09ca197d9c1
SaumyaSingh1 <ssaumyas100@gmail.com>
2021-06-29 22:04:56 +0530

    DAY 2: String and Output - fmt

---
+++ Go_Basics/stringsAndOutput.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+
+    string1 := "i am learning go,"
+    string2 := "go is compiled language,"
+    string3 := "golang is easy and has some OOPs Features"
+
+    fmt.Println(string1, string2, string3)
+}
